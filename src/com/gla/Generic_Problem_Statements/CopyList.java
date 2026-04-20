eb294252b464a0581e097b755f9251ce151278c4
sameershakya364-droid <sameershakya364@gmail.com>
2026-04-20 10:16:17 +0530

    stream-api

--- src/com/gla/Generic_Problem_Statements/CopyList.java
+++
@@ -1,7 +0,0 @@
-package src.com.gla.Generic_Problem_Statements;
-import java.util.*;
-public class CopyList {
-    static void copyList(List<? super Number> d,List<? extends Number> s){
-        d.addAll(s);
-    }
-}
--- src/com/gla/Generic_Problem_Statements/NumericSum.java
+++
@@ -1,9 +0,0 @@
-package src.com.gla.Generic_Problem_Statements;
-import java.util.*;
-public class NumericSum {
-    static double sumNumbers(List<? extends Number> list){
-        double sum=0;
-        for(Number n:list) sum+=n.doubleValue();
-        return sum;
-    }
-}
