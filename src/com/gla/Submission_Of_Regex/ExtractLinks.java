de1452beea621f444ad8b74354f3201a97bd4e68
sameershakya364-droid <sameershakya364@gmail.com>
2026-04-23 09:25:14 +0530

    Submission_Of_JDBC

--- src/com/gla/Submission_Of_Regex/ExtractLinks.java
+++
@@ -1,9 +0,0 @@
-package src.com.gla.Submission_Of_Regex;
-import java.util.regex.*;
-public class ExtractLinks {
-    public static void main(String[] args) {
-        String text="https://google.com http://test.com";
-        Matcher m=Pattern.compile("https?://\S+").matcher(text);
-        while(m.find()) System.out.println(m.group());
-    }
-}
--- src/com/gla/Submission_Of_Regex/RepeatingWords.java
+++
@@ -1,9 +0,0 @@
-package src.com.gla.Submission_Of_Regex;
-import java.util.regex.*;
-public class RepeatingWords {
-    public static void main(String[] args) {
-        String text="is is repeated repeated";
-        Matcher m=Pattern.compile("\b(\w+)\s+\1\b").matcher(text);
-        while(m.find()) System.out.println(m.group(1));
-    }
-}
